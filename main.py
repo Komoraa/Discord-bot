5f5974d20b5375f9c024dc26dc7102f8d8bf1f1d
Komora <82817981+Komoraa@users.noreply.github.com>
2025-01-03 02:55:48 +0100

    com2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(command_prefix='!', intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
 def get_temp():
     try:
         # Read the temperature from the Raspberry Pi's system file
