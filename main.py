7700942f5ff0fd08e1836a61f5aaf445181bde61
Komora <82817981+Komoraa@users.noreply.github.com>
2025-01-07 09:38:41 +0100

    restart4

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import sys
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = commands.Bot(command_prefix='!', intents=intents)
+
 def get_temp():
     try:
         with open("/sys/class/thermal/thermal_zone0/temp", "r") as file:
@@ -30,9 +30,14 @@ async def temp(ctx):
     temperature = get_temp()
     await ctx.send(f"The current Raspberry Pi temperature is {temperature}°C")
 
+@client.command()
+async def ping(ctx):
+    user = await client.fetch_user(165763943552253952)
+    await ctx.send(f"{user.mention}")
+
 try:
     client.run(token)
 except Exception as e:
     print(f"Error occurred: {e}")
-    time.sleep(60)
+    time.sleep(5)
     restart_bot()
